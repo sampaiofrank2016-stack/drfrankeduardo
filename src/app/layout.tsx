30b431177832312b3edc6ebd7d89f6c7977fd863
Frank Eduardo <sampaiofrank2016@gmail.com>
2026-07-21 21:23:11 -0300

    Configura Google Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { GoogleAnalytics } from "@next/third-parties/google";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -59,6 +60,8 @@ export default function RootLayout({
   return (
     <html lang="pt-BR" className={`${geistSans.variable} ${geistMono.variable}`}>
       <body>{children}</body>
+
+      <GoogleAnalytics gaId="G-WT98NR722X" />
     </html>
   );
 }
